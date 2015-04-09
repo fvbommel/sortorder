3f2355ac90b82b5adbaf964d0bfdf7ea4eccf406
Frits van Bommel <fvbommel@gmail.com>
2015-04-09 22:59:37 +0200

    ShortRegexpString: Add support for suffix collapsing and '?'.

--- regexp.go
+++ regexp.go
@@ -5,21 +5,19 @@ import (
 	"regexp"
 	"sort"
 	"strings"
+	"unicode/utf8"
 )
 
+var level = 0
+
 func debugf(f string, vs ...interface{}) {
-	//~ log.Printf(f, vs...)
+	// log.Printf(f, vs...)
 }
 
 // ShortRegexpString tries to construct a short regexp that matches exactly the
 // provided strings and nothing else. It assumes the strings contain no special
 // values.
 func ShortRegexpString(vs ...string) (res string) {
-
-	defer func(s string) {
-		debugf("ShortRegexpString(%s) = %#q", s, res)
-	}(fmt.Sprintf("%#q", vs))
-
 	switch len(vs) {
 	case 0:
 		return "$.^" // Unmatchable?
@@ -27,19 +25,39 @@ func ShortRegexpString(vs ...string) (res string) {
 		return regexp.QuoteMeta(vs[0])
 	}
 
-	recurse := func(prefix string, data commonSs) (result string) {
-		debugf("recurse(%#q, %v) on %#q", prefix, data, vs)
+	level++
+	defer func(s string) {
+		level--
+		debugf("%sShortRegexpString(%s) = %#q", strings.Repeat("\t", level), s, res)
+	}(fmt.Sprintf("%#q", vs))
+
+	recurse := func(substr string, data commonSs, isPrefix bool, trim func(string, string) string) (result string) {
+		debugf("recurse(%#q, %v) on %#q", substr, data, vs)
 		if data.start > 0 {
 			result += ShortRegexpString(vs[:data.start]...) + "|"
 		}
-		debugf("%v/%#q: %v\n", vs, prefix, data)
+		debugf("%v/%#q: %v\n", vs, substr, data)
 		suffixes := make([]string, data.end-data.start)
 		for i := data.start; i < data.end; i++ {
-			suffixes[i-data.start] = vs[i][len(prefix):]
+			suffixes[i-data.start] = trim(vs[i], substr)
 		}
 		middle := ShortRegexpString(suffixes...)
 		debugf(">> ShortRegexpString(%#q) = %#q", suffixes, middle)
-		result += fmt.Sprintf("%s(%s)", prefix, middle)
+		opt := ""
+		if strings.HasPrefix(middle, "|") {
+			middle = middle[1:]
+			opt = "?"
+		}
+		if len(middle) > 1 {
+			middle = fmt.Sprintf("(%s)%s", middle, opt)
+		} else {
+			middle += opt
+		}
+		if isPrefix {
+			result += fmt.Sprintf("%s%s", substr, middle)
+		} else {
+			result += fmt.Sprintf("%s%s", middle, substr)
+		}
 		if data.end < len(vs) {
 			result += "|" + ShortRegexpString(vs[data.end:]...)
 		}
@@ -50,18 +68,30 @@ func ShortRegexpString(vs ...string) (res string) {
 	for _, v := range vs {
 		bestCost += len(v) + 1
 	}
+	best := ""
 
 	found := false
+
 	prefixes := commonPrefixes(vs, 1)
-	best := ""
-	for k, v := range prefixes {
-		str := recurse(k, v)
+	for _, k := range keys(prefixes) {
+		str := recurse(k, prefixes[k], true, strings.TrimPrefix)
 		if len(str) < bestCost {
 			bestCost = len(str)
 			best = str
 			found = true
 		}
 	}
+
+	suffixes := commonSuffixes(vs, 1)
+	for _, k := range keys(suffixes) {
+		str := recurse(k, suffixes[k], false, strings.TrimSuffix)
+		if len(str) < bestCost {
+			bestCost = len(str)
+			best = str
+			found = true
+		}
+	}
+
 	if found {
 		return best
 	}
@@ -72,6 +102,15 @@ func ShortRegexpString(vs ...string) (res string) {
 		quoted[i] = regexp.QuoteMeta(vs[i])
 	}
 	return strings.Join(quoted, "|")
+}
+
+func keys(m map[string]commonSs) (result []string) {
+	result = make([]string, 0, len(m))
+	for k := range m {
+		result = append(result, k)
+	}
+	sort.Strings(result)
+	return result
 }
 
 type commonSs struct {
@@ -129,6 +168,79 @@ func prefixEnd(vs []string, prefix string) int {
 	})
 }
 
+// reverseStrings is a sort.Interface that sort strings by their reverse values.
+type reverseStrings []string
+
+func (rs reverseStrings) Less(i, j int) bool {
+	for m, n := len(rs[i])-1, len(rs[j])-1; m >= 0 && n >= 0; m, n = m-1, n-1 {
+		if rs[i][m] != rs[j][n] {
+			for ; m > 0 && !utf8.RuneStart(rs[i][m]); m-- {
+			}
+			for ; n > 0 && !utf8.RuneStart(rs[j][n]); n-- {
+			}
+			ri, _ := utf8.DecodeRuneInString(rs[i][m:])
+			rj, _ := utf8.DecodeRuneInString(rs[j][n:])
+			return ri < rj
+		}
+	}
+	return len(rs[i]) < len(rs[j])
+}
+func (rs reverseStrings) Swap(i, j int) { rs[i], rs[j] = rs[j], rs[i] }
+func (rs reverseStrings) Len() int      { return len(rs) }
+
+// commonSuffixes returns a map from prefixes to number of occurrences.
+func commonSuffixes(vs []string, minLength int) (result map[string]commonSs) {
+	sort.Sort(reverseStrings(vs))
+	debugf("Reverse-sorted: %#q", vs)
+	result = make(map[string]commonSs)
+	for i := 0; i < len(vs)-1; i++ {
+		j := i + 1
+		k := 0
+		for ; k < len(vs[i]) && k < len(vs[j]); k++ {
+			if vs[i][len(vs[i])-k-1] != vs[j][len(vs[j])-k-1] {
+				break
+			}
+		}
+		if k < minLength {
+			continue
+		}
+		suffix := vs[i][len(vs[i])-k:]
+		if _, exists := result[suffix]; !exists {
+			first := suffixStart(vs[:i], suffix)
+			// debugf("suffixStart(%#q, %#q) == %v", vs[:i], suffix, first)
+			// suffixEnd(vs, suffix) - first + 1
+			// == suffixEnd(vs[first:], suffix) + 1
+			// == suffixEnd(vs[first+1:], suffix) + 2
+			end := first + 1 + suffixEnd(vs[first+1:], suffix)
+			result[suffix] = commonSs{
+				first, end,
+			}
+			debugf("suffixEnd(%#q, %#q) == %v", vs, suffix, result[suffix].end)
+		}
+	}
+	return result
+}
+
+func suffixStart(vs []string, postfix string) int {
+	debugf("suffixStart(%#q, %#q)", vs, postfix)
+	if postfix == "" {
+		return 0
+	}
+	return findFirst(vs, func(s string) bool {
+		return strings.HasSuffix(s, postfix)
+	})
+}
+
+func suffixEnd(vs []string, postfix string) int {
+	debugf("suffixEnd(%#q, %#q)", vs, postfix)
+	if postfix == "" {
+		return len(vs)
+	}
+	return findFirst(vs, func(s string) bool {
+		return !strings.HasSuffix(s, postfix)
+	})
+}
+
 func findFirst(vs []string, predicate func(string) bool) int {
 	l, h := -1, len(vs)
 	// Invariant: vs[l] does not match, vs[h] does.
@@ -142,5 +254,6 @@ func findFirst(vs []string, predicate func(string) bool) int {
 			l = m
 		}
 	}
+	debugf("==> %d", h)
 	return h
 }
--- regexp_test.go
+++ regexp_test.go
@@ -8,12 +8,17 @@ func TestShortRegexpString(t *testing.T) {
 		out string
 	}{
 		{[]string{"abc", "def"}, "abc|def"},
-		{[]string{"abcf", "abde"}, "ab(cf|de)"},
-		{[]string{"abcdefabcf", "abcdefabde", "abc"}, "abc(|defab(cf|de))"},
-		{[]string{"css/bootstrap.css", "css/bootstrap.min.css", "css/bootstrap-theme.css", "css/bootstrap-theme.min.css", "css/main.css", "css/normalize.css", "css/pygment_highlights.css", "feed.xml", "img/avatar-icon.png", "js/bootstrap.js", "js/bootstrap.min.js", "js/jquery-1.11.2.min.js", "js/main.js"}, `css/(bootstrap(-theme.(css|min\.css)|.(css|min\.css))|main\.css|normalize\.css|pygment_highlights\.css)|feed\.xml|img/avatar-icon\.png|js/(bootstrap.(js|min\.js)|jquery-1\.11\.2\.min\.js|main\.js)`},
+		{[]string{"abcf", "abde"}, "ab(de|cf)"},
+		{[]string{"abcdefabcf", "abcdefabde", "abc"}, "abc(defab(de|cf))?"},
+		{[]string{"css/bootstrap.css", "css/bootstrap.min.css", "css/bootstrap-theme.css", "css/bootstrap-theme.min.css"},
+			`css/bootstrap((-theme(\.min)?|\.min)?.css)`},
+		// `css/bootstrap(-theme)?(\.min)?.css)`},	// Ideal
+		{[]string{"css/bootstrap.css", "css/bootstrap.min.css", "css/bootstrap-theme.css", "css/bootstrap-theme.min.css", "css/main.css", "css/normalize.css", "css/pygment_highlights.css", "feed.xml", "img/avatar-icon.png", "js/bootstrap.js", "js/bootstrap.min.js", "js/jquery-1.11.2.min.js", "js/main.js"},
+			`css/((bootstrap(-theme(\.min)?|\.min)?|normalize|main|pygment_highlights).css)|img/avatar-icon\.png|feed\.xml|js/((bootstrap(\.min)?|main|jquery-1\.11\.2\.min).js)`},
+		// `css/(bootstrap(-theme(\.min)?|\.min)?|normalize|main|pygment_highlights).css|img/avatar-icon\.png|feed\.xml|js/(bootstrap(\.min)?|main|jquery-1\.11\.2\.min).js`}, // Ideal
 	} {
 		if got := ShortRegexpString(test.in...); got != test.out {
-			t.Errorf("expected: %#q, got %#q for\n\t%v", test.out, got, test.in)
+			t.Errorf("expected:\n\t%#q,\ngot\n\t%#q for\n\t%v", test.out, got, test.in)
 		}
 	}
 }
@@ -30,6 +35,34 @@ func TestCommonPrefixes(t *testing.T) {
 			map[string]int{"abc": 2, "ab": 3}},
 	} {
 		got := commonPrefixes(test.in, 2)
+		want := test.out
+		if len(got) != len(want) {
+			t.Errorf("expected: %v, got %v for\n\t%v", test.out, got, test.in)
+			continue
+		}
+		for k, v := range want {
+			if got[k].end-got[k].start != v {
+				t.Errorf("expected: %v, got %v for\n\t%v", test.out, got, test.in)
+				break
+			}
+		}
+	}
+}
+
+func TestCommonSuffixes(t *testing.T) {
+	for _, test := range []struct {
+		in  []string
+		out map[string]int
+	}{
+		{[]string{"abc", "def"}, nil},
+		{[]string{"fcba", "edba"},
+			map[string]int{"ba": 2}},
+		{[]string{"fcba", "decba", "dba"},
+			map[string]int{"cba": 2, "ba": 3}},
+	} {
+		// log.Print(test)
+		got := commonSuffixes(test.in, 2)
+		// log.Print(got)
 		want := test.out
 		if len(got) != len(want) {
 			t.Errorf("expected: %v, got %v for\n\t%v", test.out, got, test.in)
