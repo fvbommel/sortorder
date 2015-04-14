357093d9725d0a2e9599c9b62f18100d8d04b3a6
Frits van Bommel <fvbommel@gmail.com>
2015-04-14 21:03:46 +0200

    Fix sortorder package-doc snafu.

--- sortorder/doc.go
+++ sortorder/doc.go
@@ -1,5 +1,5 @@
 // Package sortorder implements sort orders and comparison functions.
 //
-// Currently, it only implements so-called "natural order", where strings
-// embedded in integers are compared by value.
+// Currently, it only implements so-called "natural order", where integers
+// embedded in strings are compared by value.
 package sortorder
